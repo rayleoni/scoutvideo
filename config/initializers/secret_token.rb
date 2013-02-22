a650cd55b1672f33387959a2816a8ab504067a26
Ray Lee <ray@heyscout.com>
2013-02-22 15:20:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoutVideo::Application.config.secret_token = '2fc1b73fbe6e7a5fa73723ac9e1a5d279d227af148f0401920f13d1b3c9bd5453a6d3d2dc4ef0c70324a79e6ab00a97bf4ae999902cbaeed208470524b400be5'
